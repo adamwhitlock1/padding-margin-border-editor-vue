4ca8a9cca7e97843b573cb1759dd221a9653acaa
Adam Whitlock <moose62712@gmail.com>
2019-03-01 12:18:12 -0700

    added all sides and box sizing for preview

--- src/components/Styles.vue
+++ src/components/Styles.vue
@@ -1,40 +1,42 @@
 <template>
   <div class="hello">
-    <input class="padding-top" type="text">
-    <input class="padding-right" type="text">
-    <input class="padding-bottom" type="text">
-    <input class="padding-left" type="text">
+    <input class="padding-top" type="number" v-model="paddingTop" />
+    <input class="padding-right" type="number" v-model="paddingRight" />
+    <input class="padding-bottom" type="number" v-model="paddingBottom" />
+    <input class="padding-left" type="number" v-model="paddingLeft" />
 
-    <div class="preview" :style={border-size-top: "padding-top"} >
-    </div>
-
-
+    <div
+      class="preview"
+      :style="{
+        'border-top-width': paddingTop + 'px',
+        'border-right-width': paddingRight + 'px',
+        'border-bottom-width': paddingBottom + 'px',
+        'border-left-width': paddingLeft + 'px'
+      }"
+    ></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Styles",
-  props: {
-    msg: String
+  data() {
+    return {
+      paddingTop: 1,
+      paddingRight: 1,
+      paddingBottom: 1,
+      paddingLeft: 1
+    };
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.preview {
+  border: 1px solid blue;
+  width: 200px;
+  height: 200px;
+  margin: 20px auto;
 }
 </style>
